3f15f152dddf50800da4ea5b6173c5508b3c7917
Mark Tiddy <marktiddy@gmail.com>
2020-01-24 18:15:41 +0000

    allowing all users to access movies

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+class MainView extends React.Component {
+  constructor() {
+    super();
+    //initialise the state so we can get to it later
+    this.state = {};
+  }
+
+  //Next override the render method
+  render() {
+    return <div className="main-view"></div>;
+  }
+}
