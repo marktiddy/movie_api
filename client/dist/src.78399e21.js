bedb2fec74d7cbb23f7fb4f392bfa6fbd4a18b37
Mark Tiddy <marktiddy@gmail.com>
2020-02-05 21:23:16 +0000

    API fully working

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -52928,7 +52928,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63860" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50325" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
